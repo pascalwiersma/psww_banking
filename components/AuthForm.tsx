093da3d263287d0d7b11ea4f9e6d53546992d375
Pascal <pascal@psww.nl>
2024-08-05 17:41:42 +0200

    Implement plaid link and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,7 @@ import { Form } from "./ui/form";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: "sign-in" | "sign-up" }) => {
     const router = useRouter();
@@ -42,8 +43,22 @@ const AuthForm = ({ type }: { type: "sign-in" | "sign-up" }) => {
         setisLoading(true);
         
         try {
+
             if(type === "sign-up") {
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                };    
+             
+                const newUser = await signUp(userData);
 
                 setUser(newUser);
 
@@ -99,7 +114,9 @@ const AuthForm = ({ type }: { type: "sign-in" | "sign-up" }) => {
                 </div>
             </header>
             {user ? (
-                <div className="flex flex-col gap-4">{/* Plaid link */}</div>
+                <div className="flex flex-col gap-4">
+                    <PlaidLink user={user} variant="primary" />
+                </div>
             ) : (
                 <>
                     <Form {...form}>
